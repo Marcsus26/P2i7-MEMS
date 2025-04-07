8d2d2f12d202c820f728c45d77f6ce2bd409e9ac
tristanhp <tristan.hausermann-pernet@insa-lyon.fr>
2025-04-07 16:53:34 +0200

    ajout courbe reponse (mais ça marche pas :/ )

--- newmark.py
+++ newmark.py
@@ -21,7 +21,7 @@ def calc_dFnl(T,Vdc,y,dY=0):
     return dFY,dFdY
 
 #Fonction newmark
-def Newmark(X0,dX0,t_init,dt,NT,omega0):
+def Newmark(Y0,dY0,t_init,dt,NT,omega0):
     precNR=1.e-9
     # C.I. déplacement et vitesse
     t = t_init
@@ -76,8 +76,8 @@ def Newmark(X0,dX0,t_init,dt,NT,omega0):
             #normres=abs(res/P)
             normres=abs(deltaY/Y)
         tt[n]=t     # stockage t(n)
-        Yt[n]=Y     # stockage X(n)
-        dYt[n]=dY  # stockage dX(n)
+        Yt[n]=Y     # stockage Y(n)
+        dYt[n]=dY  # stockage dY(n)
     return tt,Yt,dYt
 
 #Déclaration des parametres du système
@@ -110,13 +110,13 @@ OMEGA = 1
 periode=2*np.pi/OMEGA  # periode de l'excitation et de la reponse
 nb_pts_per = 50          # nb de points par periode pour l integration temporelle
 dt=periode/nb_pts_per  # taille du pas de temps
-nb_per = 2000              # nb de periodes pour le calcul temporel
+nb_per = 5000              # nb de periodes pour le calcul temporel
 t_tot=nb_per*periode   # temps final
 t_init=0               # temps initial
 NT=nb_per*nb_pts_per+1 # Nb total de pas de temps
 
 # Résolution equation mvt
-
+"""
 OMEGA=1
 
 Y0=1 ; dY0=0             # conditions initiales
@@ -126,10 +126,78 @@ print('ellapsed time: ',time.process_time() - start)
 
 fig = plt.figure(figsize=(10, 4))  # Crée une figure de dimensions données.
 ax = fig.add_subplot()
-ax.plot(tt, Yt, color='red',marker = 'o', label='Newmark')  # Trace le deplacement en fonction du temps.
+ax.plot(tt, Yt, color='red',marker = '.', label='Newmark')  # Trace le deplacement en fonction du temps.
 plt.xlabel("Temps $t$")        # titre axe horizontal
 plt.ylabel("Déplacement $y(t)$")  # titre axe vertical
 plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
 plt.show()
+"""
+# Courbe de réponse
 
-#
+OMEGA_debut=0.985;OMEGA_fin=1.005;dOMEGA=0.005
+nb_pts_per=50          # nb de points par periode pour l integration temporelle
+nb_per=50              # nb de periodes pour le calcul temporel
+t_init=0               # temps initial
+NT=nb_per*nb_pts_per   # Nb total de pas de temps
+npas=int((OMEGA_fin-OMEGA_debut)/dOMEGA+1)
+# conditions initiales
+Y0=1;dY0=0
+k=0; OMEGA=OMEGA_debut
+
+OME=zeros((npas,1))
+AMPL=zeros((npas,1))
+
+# boucle sur Omega 
+while (OMEGA<OMEGA_fin):
+    OME[k]=OMEGA
+    periode=2*np.pi/OMEGA   # periode de l'excitation et de la reponse
+    dt=periode/nb_pts_per   # mise a jour du pas de temps
+    t_tot=nb_per*periode    # mise a jour du temps final
+    tt,Yt,dYt=Newmark(Y0,dY0,t_init,dt,NT,omega0)   # Integration par Newmark
+    AMPL[k]=max(Yt[-3*nb_pts_per:])   # recherche de l’amplitude max
+    print(f'ome= {OME[k,0]:0.5f}  y= {AMPL[k,0]:0.5g}', end="\r", flush=True)   # affichage resultat
+    Y0=Yt[-1,0];dY0=dYt[-1,0]   # nouvelles CI
+    OMEGA=OMEGA+dOMEGA
+    k=k+1
+
+fig = plt.figure(figsize=(10, 4))  # Crée une figure de dimensions données.
+ax = fig.add_subplot()
+ax.plot(OME, AMPL, color='red',marker = '>', label='montée en fréquence')  # Trace la courbe de réponse.
+plt.xlabel("$\Omega$ pulsation de l'excitation")
+plt.ylabel("Amplitude de la réponse = $max(y(t))$")
+plt.title("Courbe de réponse")
+plt.legend()
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+
+k=0; OMEGA=OMEGA_fin
+
+npas=int((OMEGA_fin-OMEGA_debut)/dOMEGA+1)
+#OMEGA=np.linspace(OMEGA_debut, OMEGA_fin, num=npas)
+OME2=zeros((npas,1))
+AMPL2=zeros((npas,1))
+# boucle sur Omega 
+start = time.process_time()
+while (OMEGA>OMEGA_debut):
+    OME2[k]=OMEGA
+    periode=2*np.pi/OMEGA      # periode de l'excitation et de la reponse
+    dt=periode/nb_pts_per  # taille du pas de temps
+    t_tot=nb_per*periode   # temps final
+    tt,Yt,dYt=Newmark(Y0,dY0,t_init,dt,NT,omega0)   # Integration par Newmark
+    AMPL2[k]=max(Yt[-3*nb_pts_per:])  # recherche de l’amplitude max
+    print(f'ome= {OME2[k,0]:0.5f}  x= {AMPL2[k,0]:0.5g}', end="\r", flush=True)   # affichage resultat
+    Y0=Yt[-1,0];dY0=dYt[-1,0]   # nouvelles CI
+    OMEGA=OMEGA-dOMEGA
+    k=k+1
+
+ax.plot(OME2, AMPL2, color='blue',marker = '<', label='descente en fréquence')  # Trace la courbe de réponse.
+
+data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
+OMEGA_data = data[:, 0]
+AMPL_data = data[:, 1]
+
+# Ajout des données du fichier au graphe existant
+ax.plot(OMEGA_data, AMPL_data, color='green', marker='o', label='données fichier')  # Trace les données du fichier.
+
+plt.legend()
+
+plt.show()
