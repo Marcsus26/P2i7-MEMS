6d97a60cb000bd850ad612c4e3fc69d6224d0ba6
tristanhp <tristan.hausermann-pernet@insa-lyon.fr>
2025-04-09 09:22:28 +0200

    few changes

--- newmark masse_ajoutee.py
+++ newmark masse_ajoutee.py
@@ -9,6 +9,7 @@ import newmark as nk
 T, Vdc, Vac, omega0, M, C, K = nk.init_params()
 OMEGA_debut, OMEGA_fin, dOMEGA = 0.9, 1.10, 0.005
 nb_pts_per, nb_per = 50, 500
+fig,ax = plt.subplots(1,1,figsize=(10,4))
 
 OME, AMPL = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_debut, OMEGA_fin, dOMEGA, nb_pts_per, nb_per)
 
@@ -20,9 +21,9 @@ data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
 OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
 
 # Affichage
-nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data)
-
-T, Vdc, Vac, omega0, M, C, K = nk.init_params(1e-13)
+nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data,ax)
+deltam = 1e-13
+T, Vdc, Vac, omega0, M, C, K = nk.init_params(deltam)
 OMEGA_debut, OMEGA_fin, dOMEGA = 0.9, 1.10, 0.005
 nb_pts_per, nb_per = 50, 500
 
@@ -36,4 +37,6 @@ data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
 OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
 
 # Affichage
-nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data)
+nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data,ax,deltam)
+
+plt.show()
