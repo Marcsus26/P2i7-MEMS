353825286c6764f998a2027c9bcc5a0738995eeb
Marcd <marc.doumit@insa-lyon.fr>
2025-04-14 17:10:17 +0200

    fixed saut

--- newmark masse_ajoutee.py
+++ newmark masse_ajoutee.py
@@ -6,7 +6,7 @@ import time
 
 import newmark as nk
 
-OMEGA_debut, OMEGA_fin, dOMEGA = 0.985, 1, 0.0005
+OMEGA_debut, OMEGA_fin, dOMEGA = 0.985, 1, 0.0003
 
 
 T, Vdc, Vac, omega0, M, C, K = nk.init_params()
@@ -15,12 +15,11 @@ fig,ax = plt.subplots(1,1,figsize=(10,4))
 
 OME, AMPL = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_debut, OMEGA_fin, dOMEGA, nb_pts_per, nb_per)
 
-
-
     
 # Descente en fréquence
 OME2, AMPL2 = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_fin, OMEGA_debut, -dOMEGA, nb_pts_per, nb_per)
 
+val1 = OME2[np.argmax(AMPL2)]
 # Chargement des données de la courbe de réponse
 data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
 OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
@@ -38,6 +37,7 @@ OME, AMPL = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_debut,
 # Descente en fréquence
 OME2, AMPL2 = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_fin, OMEGA_debut, -dOMEGA, nb_pts_per, nb_per)
 
+val2 = OME2[np.argmax(AMPL2)]
 # Chargement des données de la courbe de réponse
 data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
 OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
@@ -46,8 +46,9 @@ OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
 nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data,ax,deltam, True)
 
 
-# print(val1)
-# print(val2)
-# print(f"La différence de OMEGA est de : {abs(val1-val2)[0]}")
+print(val1)
+print(val2)
+print(f"La différence de OMEGA est de : {abs(val1-val2)[0]}")
 
 plt.show()
+
