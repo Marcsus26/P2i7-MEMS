5f7adeec4573936a6a4ab575849b9ca308637e51
Marcd <marc.doumit@insa-lyon.fr>
2025-05-07 11:51:24 +0200

    saut marche

--- newmark_masse_ajoutee.py
+++ newmark_masse_ajoutee.py
@@ -44,4 +44,26 @@ def courbe_reponse_delta_m(OMEGA_debut, OMEGA_fin, dOMEGA, deltam=0, montee=Fals
 
     # Affichage
     # nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data,ax,deltam, True)
-    return OMEGA_MAX
+    return OMEGA_MAX, OME, AMPL, OME2, AMPL2
+
+def affichage_courbe_rep():
+    Omega_max, OME, AMPL, OME2, AMPL2 = courbe_reponse_delta_m(0.985, 1, 0.0003, 0, True)
+    Omega_max, OME13, AMPL13, OME213, AMPL213 = courbe_reponse_delta_m(0.985, 1, 0.0003, 1e-13, True)
+    data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
+    OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
+    fig,ax = plt.subplots(1,1,figsize=(10,4))
+    ax.plot(OME, AMPL, label='Montee')
+    ax.plot(OME2, AMPL2, label='Descente')
+    ax.plot(OME13, AMPL13, label='Montee avec deltam=1e-13')
+    ax.plot(OME213, AMPL213, label='Descente avec deltam=1e-13')
+    ax.plot(OMEGA_data, AMPL_data, label='Données expérimentales', linestyle='--')
+    ax.legend()
+    ax.set_xlabel('OMEGA')
+    ax.set_ylabel('AMPL')
+    ax.set_title('Courbe de réponse avec et sans masse ajoutée')
+    ax.grid()
+
+
+
+affichage_courbe_rep()
+plt.show()
--- saut.py
+++ saut.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Initialize parameters
 T, Vdc, Vac, omega0, M, C, K = nk.init_params()
 
-OMEGA = 0.9925
+OMEGA = 0.9913
 periode = 2 * np.pi / OMEGA  # Period of excitation and response
 nb_pts_per = 60             # Number of points per period for time integration
 dt = periode / nb_pts_per   # Time step size
@@ -32,15 +32,14 @@ tt, Yt, dYt = nk.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA, M,
 
 
 # Verify the amplitude of the periodic solution
-amplitude_periodic_solution = np.max(Yt[-2 * nb_pts_per:])
+amplitude_periodic_solution = np.max(Yt[-3 * nb_pts_per:])
 print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 #assert np.isclose(amplitude_periodic_solution, 0.169, atol=0.01), "The amplitude does not match the expected value of 0.169."
 
 # Add a small mass and continue integration
-deltam = 1e-13
 
 # Update parameters with the added mass
-M_new = M + deltam
+T, Vdc, Vac, omega0, M_new, C, K = nk.init_params(1e-13)
 
 # Use final conditions from the previous integration as initial conditions
 Y0_new = Yt[-1]
@@ -49,7 +48,7 @@ dY0_new = dYt[-1]
 # Perform Newmark integration for the extended time period
 tt_new, Yt_new, dYt_new = nk.Newmark(Y0_new, dY0_new, t_tot, dt, NT, omega0, T, Vdc, Vac, OMEGA, M_new, C, K)
 
-amplitude_periodic_solution = np.max(Yt_new[-2 * nb_pts_per:])
+amplitude_periodic_solution = np.max(Yt_new[-3 * nb_pts_per:])
 print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 
 # Plot the displacement over time for both integrations
@@ -63,14 +62,14 @@ print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 # plt.grid()
 # plt.show()
 
-
-M_new -= deltam
+deltam = 1e-13
+T, Vdc, Vac, omega0, M_new, C, K = nk.init_params()
 Y0_new2 = Yt_new[-1]
 dY0_new2 = dYt_new[-1]
 
 tt_new2, Yt_new2, dYt_new2 = nk.Newmark(Y0_new2, dY0_new2, 2*t_tot, dt, NT, omega0, T, Vdc, Vac, OMEGA, M_new, C, K)
 
-amplitude_periodic_solution = np.max(Yt_new2[-2 * nb_pts_per:])
+amplitude_periodic_solution = np.max(Yt_new2[-3 * nb_pts_per:])
 print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 
 # plt.figure(figsize=(10, 6))
@@ -84,21 +83,20 @@ print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 # plt.grid()
 # plt.show()
 
-deltam = 1e-14
-M_new += deltam
+T, Vdc, Vac, omega0, M_new, C, K = nk.init_params(1e-14)
 Y0_new3 = Yt_new2[-1]
 dY0_new3 = dYt_new2[-1]
 
 tt_new3, Yt_new3, dYt_new3 = nk.Newmark(Y0_new3, dY0_new3, 3*t_tot, dt, NT, omega0, T, Vdc, Vac, OMEGA, M_new, C, K)
 
-amplitude_periodic_solution = np.max(Yt_new3[-2 * nb_pts_per:])
+amplitude_periodic_solution = np.max(Yt_new3[-3 * nb_pts_per:])
 print(f"Amplitude of the periodic solution: {amplitude_periodic_solution}")
 
 plt.figure(figsize=(10, 6))
-plt.plot(tt, Yt, label='Displacement (Yt) - deltam=0')
-plt.plot(tt_new, Yt_new, label='Displacement (Yt) - deltam=1e-13', color='orange')
-plt.plot(tt_new2, Yt_new2, label='Displacement (Yt) - deltam=0')
-plt.plot(tt_new3, Yt_new3, label='Displacement (Yt) - deltam=1e-14')
+plt.plot(tt/(omega0), Yt, label='Displacement (Yt) - deltam=0')
+plt.plot(tt_new/omega0, Yt_new, label='Displacement (Yt) - deltam=1e-13', color='orange')
+plt.plot(tt_new2/omega0, Yt_new2, label='Displacement (Yt) - deltam=0')
+plt.plot(tt_new3/omega0, Yt_new3, label='Displacement (Yt) - deltam=1e-14')
 plt.xlabel('Time (s)')
 plt.ylabel('Displacement')
 plt.title('Displacement Over Time with and without Added Mass')
