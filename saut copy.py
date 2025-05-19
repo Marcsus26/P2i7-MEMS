1c2382a018bcdaff61dbc881f5bdbea369d9840b
Marcd <marc.doumit@insa-lyon.fr>
2025-05-19 17:28:06 +0200

    minor changes

--- saut copy.py
+++ saut copy.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
-    OMEGA = 0.9923
+    OMEGA = 0.9928
     periode = 2 * np.pi / OMEGA  # Period of excitation and response
     nb_pts_per = 60             # Number of points per period for time integration
     dt = periode / nb_pts_per   # Time step size
@@ -19,6 +19,7 @@ def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
     fig,ax = plt.subplots(figsize = (16,9))
 
     for i in range (len(l_masses)):
+        OMEGA +=0.0005
         for omega in range(-50, 50, 1):
             tt, Yt, dYt = nk.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA+(omega*1e-3), M0, C, K)
             if np.max(Yt[-3 * nb_pts_per:]) < 0.2:
@@ -34,15 +35,13 @@ def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
         Y0 = Yt[-1]
         dY0 = dYt[-1]
         t_init = tt[-1]
-
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Displacement')
     ax.set_title('Displacement Over Time with and without Added Mass')
-    ax.legend()
     ax.grid()
     plt.show()
 
 # Initialize parameters
 T, Vdc, Vac, omega0, M, C, K = nk.init_params()
-liste_masse = [1e-13,1e-14, 1e-15, 1e-16, 1e-17, 1e-18]
+liste_masse = [1e-14, 1e-15, 1e-16, 1e-17, 1e-18, 1e-19, 1e-20, 1e-21, 1e-22, 1e-23, 1e-24, 1e-25]
 saut_amp(T, Vdc, Vac, omega0, M, C, K, liste_masse)
