cbb4436ee319a0beb65f222d449a352ed1c7a28a
Tthomma <thomb13@free.fr>
2025-05-21 11:45:48 +0200

    ajout test2

---
+++ test2.py
@@ -0,0 +1,26 @@
+from newmark_saut import *
+
+T, Vdc, Vac, omega0, M, C, K, OMEGA_bal = init_params()
+
+# Initialize parameters
+
+periode = 2 * np.pi / OMEGA_bal  # Period of excitation and response
+nb_pts_per = 30         # Number of points per period for time integration
+dt = periode / nb_pts_per   # Time step size
+nb_per = 500000            # Number of periods for time integration
+t_tot = nb_per * periode    # Final time
+t_init = 0                  # Initial time
+NT = nb_per * nb_pts_per
+
+tt, Yt, dYt = Newmark(0.3, 0.3, t_init, dt, NT, omega0, T, Vdc, Vac, 0.99, 0.993, M, C, K, OMEGA_bal)
+
+# Plot the displacement over time
+plt.figure(figsize=(50, 6))
+plt.plot(tt / omega0, Yt, label='Déplacement (Yt)')
+plt.xlabel('Temps (s)')
+plt.ylabel('Déplacement')
+plt.title('Déplacement au cours du temps pour deltam = 0')
+plt.legend()
+plt.xlim(0, 2e-5)
+plt.grid()
+plt.show()
