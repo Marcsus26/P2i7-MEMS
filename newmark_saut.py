9f03aaf535d40e41bd520fba8264b034f0b2b678
Marcd <marc.doumit@insa-lyon.fr>
2025-05-21 09:13:00 +0200

    changed saut for baguet

---
+++ newmark_saut.py
@@ -0,0 +1,125 @@
+from numba import njit
+import numpy as np
+from numpy import zeros
+import matplotlib.pyplot as plt
+
+# Define functions with numba acceleration
+@njit
+def calc_P(T, Vdc, Vac, OMEGA_min, OMEGA_max, t, OMEGA_bal):
+    OMEGA_c = (OMEGA_max + OMEGA_min)/2
+    delta_OMEGA = (OMEGA_max - OMEGA_min)/2
+    theta = OMEGA_c + (delta_OMEGA/OMEGA_bal)*np.cos(OMEGA_bal*t)
+    return T * Vdc**2 + 2 * T * Vdc * Vac * np.cos(theta)
+
+@njit
+def calc_Fnl(T, Vdc, y):
+    return -(3 * T * Vdc**2) * y**2 - (4 * T * Vdc**2) * y**3
+
+@njit
+def calc_dFnl(T, Vdc, y):
+    dFY = -6 * T * (Vdc**2) * (y + 2 * y**2)
+    dFdY = 0
+    return dFY, dFdY
+
+@njit
+def Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA, M, C, K, OMEGA_bal):
+    precNR = 1.e-12
+    t = t_init
+    Y, dY = Y0, dY0
+    tt, Yt, dYt = zeros((NT, 1)), zeros((NT, 1)), zeros((NT, 1))
+    Fnl = calc_Fnl(T, Vdc, Y)
+
+    tt[0], Yt[0], dYt[0] = t, Y, dY
+
+    for n in range(1, NT):
+        P = calc_P(T, Vdc, Vac, 0.99, 0.993, t, OMEGA_bal)
+        ddY = (P - C * dY - K * Y - Fnl) / M
+        t += dt
+        iter = 0
+        Y += dt * dY + (dt**2 / 2) * ddY
+        dY += dt * ddY
+        res = P - M * ddY - C * dY - K * Y - calc_Fnl(T, Vdc, Y)
+        normres = np.abs(res / P)  # Use numpy.abs for array compatibility
+
+        while normres > precNR:
+            iter += 1
+            dFY, dFdY = calc_dFnl(T, Vdc, Y)
+            J = (4 / dt**2) * M + (2 / dt) * (C + dFdY) + K + dFY
+            deltaY = res / J
+            Y += deltaY
+            dY += (2 / dt) * deltaY
+            ddY += (4 / dt**2) * deltaY
+            res = P - M * ddY - C * dY - K * Y - calc_Fnl(T, Vdc, Y)
+            normres = np.abs(deltaY / Y)
+
+        tt[n], Yt[n], dYt[n] = t, Y, dY
+
+    return tt, Yt, dYt
+
+@njit
+def compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_debut, OMEGA_fin, dOMEGAinit, nb_pts_per, nb_per, OMEGA_bal, tolerance=0.00001):
+    npas = int(abs((OMEGA_fin - OMEGA_debut) / dOMEGAinit) + 1)
+    OME, AMPL = zeros((npas, 1)), zeros((npas, 1))
+    Y0, dY0 = 0.25, 0.25
+    k, OMEGA = 0, OMEGA_debut
+
+    while (dOMEGAinit > 0 and OMEGA <= OMEGA_fin) or (dOMEGAinit < 0 and OMEGA >= OMEGA_fin):
+        OME[k] = OMEGA
+        periode = 2 * np.pi / OMEGA
+        dt = periode / nb_pts_per
+        NT = nb_per * nb_pts_per
+        tt, Yt, dYt = Newmark(Y0, dY0, 0, dt, NT, omega0, T, Vdc, Vac, OMEGA, M, C, K, OMEGA_bal)
+        AMPL[k] = max(Yt[-3 * nb_pts_per:])
+        Y0, dY0 = Yt[-1, 0], dYt[-1, 0]
+        OMEGA += dOMEGAinit
+        k += 1
+    
+    val = -1000
+
+    if dOMEGAinit < 0:
+        i_max = 0
+        for i in range(len(AMPL)):
+            if AMPL[i] > AMPL[i_max]:
+                i_max = i
+        val = OME[i_max]
+
+        npas = int(abs((OMEGA_fin - OMEGA_debut) / dOMEGAinit) + 1 + (0.002) / tolerance)
+        OME, AMPL = zeros((npas, 1)), zeros((npas, 1))
+        Y0, dY0 = 0.25, 0
+        k, OMEGA = 0, OMEGA_debut
+
+        while (dOMEGAinit > 0 and OMEGA <= OMEGA_fin) or (dOMEGAinit < 0 and OMEGA >= OMEGA_fin):
+            OME[k] = OMEGA
+            if (val - 0.001) < OME[k] and (val + 0.001) > OME[k]:
+                dOMEGA = -tolerance
+            else:
+                dOMEGA = dOMEGAinit
+            periode = 2 * np.pi / OMEGA
+            dt = periode / nb_pts_per
+            NT = nb_per * nb_pts_per
+            tt, Yt, dYt = Newmark(Y0, dY0, 0, dt, NT, omega0, T, Vdc, Vac, OMEGA, M, C, K, OMEGA_bal)
+            AMPL[k] = max(Yt[-3 * nb_pts_per:])
+            Y0, dY0 = Yt[-1, 0], dYt[-1, 0]
+            OMEGA += dOMEGA
+            k += 1
+
+    return OME[:k], AMPL[:k]
+
+@njit
+def init_params(deltam=0):
+    rho, l, b, h, d = 2500, 250e-6, 40e-6, 1e-6, 0.03e-6
+    Vdc, Vac = 5, 5 / 10
+    epsilon0 = 8.5e-12
+    f0 = 100e6
+    omega0 = 2 * np.pi * f0
+    Q = 1000
+    xi = 1 / Q
+    A = l * b
+    m = rho * A * h
+    T = (epsilon0 * A) / (2 * m * omega0**2 * d**3)
+    M = 1 + (deltam / m)
+    C = xi
+    K = 1 - 2 * T * Vdc**2
+    OMEGA_bal = 2*np.pi*50 / omega0
+    return T, Vdc, Vac, omega0, M, C, K, OMEGA_bal
+
--- saut_final.py
+++ saut_final.py
@@ -1,9 +1,9 @@
-import newmark as nk
 import numpy as np
 import matplotlib.pyplot as plt
+import newmark_saut as nks
 
 def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
-    OMEGA = 0.9933
+    OMEGA = 0.9928
     periode = 2 * np.pi / OMEGA  # Period of excitation and response
     nb_pts_per = 60             # Number of points per period for time integration
     dt = periode / nb_pts_per   # Time step size
@@ -19,25 +19,19 @@ def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
     fig,ax = plt.subplots(figsize = (16,9))
 
     for i in range(len(l_masses)):
-        if l_masses[i] >= 1e-14:
-            OMEGA = 0.9933 +  (0.0005*np.log10(l_masses[i]/1e-14)) # Frequency of excitation
-        else:
-            OMEGA = 0.9933 +  (0.0005*np.log10(1e-14/l_masses[i]))
-        for omega in range(-50, 50, 1):
-            tt, Yt, dYt = nk.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA+(omega*1e-3), M0, C, K)
-            if np.max(Yt[-3 * nb_pts_per:]) < 0.2:
-                break
-        T, Vdc, Vac, omega0, M_new, C, K = nk.init_params(l_masses[i])
+        T, Vdc, Vac, omega0, M_new, C, K, OMEGA_bal = nks.init_params(l_masses[i])
+        tt, Yt, dYt = nks.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA, M0, C, K, OMEGA_bal)
         ax.plot(tt/(omega0), Yt, label='Displacement (Yt) - deltam=0')
         t_init = tt[-1]
         Y0 = Yt[-1]
         dY0 = dYt[-1]
-        tt, Yt, dYt = nk.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA, M_new, C, K)
+        tt, Yt, dYt = nks.Newmark(Y0, dY0, t_init, dt, NT, omega0, T, Vdc, Vac, OMEGA, M_new, C, K, OMEGA_bal)
         print(np.max(Yt[-3 * nb_pts_per:]))
         ax.plot(tt/(omega0), Yt, label=f'Displacement (Yt) - deltam={l_masses[i]}')
         Y0 = Yt[-1]
         dY0 = dYt[-1]
         t_init = tt[-1]
+    
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Displacement')
     ax.set_title('Displacement Over Time with and without Added Mass')
@@ -45,7 +39,7 @@ def saut_amp(T, Vdc, Vac, omega0, M, C, K, l_masses):
     plt.show()
 
 # Initialize parameters
-T, Vdc, Vac, omega0, M, C, K = nk.init_params()
-# liste_masse = [1e-14, 1e-15, 1e-16, 1e-17, 1e-18, 1e-19, 1e-20, 1e-21, 1e-22, 1e-23, 1e-24, 1e-25]
-liste_masse = [1e-25, 1e-14]
+T, Vdc, Vac, omega0, M, C, K, OMEGA_bal = nks.init_params()
+liste_masse = [1e-13,0]
+# liste_masse = [1e-25, 1e-14]
 saut_amp(T, Vdc, Vac, omega0, M, C, K, liste_masse)
