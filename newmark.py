75894fc481043cbcf10d35aa13a42ba1b2f8e84e
Taihani Chan <taihanichan@MacBook-Pro-de-Taihani.local>
2025-05-26 16:07:09 +0200

    modif jupyter

--- newmark.py
+++ newmark.py
@@ -104,32 +104,16 @@ def compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_debut, OMEGA_fin,
 
 def plot_response_curve(OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data, ax, deltam=0, tracer_data=False):
 
-
-    ax.plot(OME, AMPL, marker='>', label=f'montée en fréquence pour {deltam}')
-
-
-    ax.plot(OME2, AMPL2, marker='<', label=f'descente en fréquence pour {deltam}')
-
     if tracer_data:
-
         ax.plot(OMEGA_data, AMPL_data, color='green', marker='o', label='données fichier')
 
-
     ax.plot(OME, AMPL, marker='>', label=f'montée en fréquence pour {deltam}')
-
-
     ax.plot(OME2, AMPL2, marker='<', label=f'descente en fréquence pour {deltam}')
-
     plt.xlabel(r"$\Omega$ pulsation de l'excitation")
-
     plt.ylabel("Amplitude de la réponse = $max(y(t))$")
-
     plt.title("Courbe de réponse")
-
     plt.legend()
-
     plt.xlim(0.988,0.996)
-
     plt.grid(color='gray', linestyle='--', linewidth=0.5)
 
 @njit(fastmath=True)
--- newmark_masse_ajoutee.py
+++ newmark_masse_ajoutee.py
@@ -18,9 +18,4 @@ def courbe_reponse_delta_m(OMEGA_debut, OMEGA_fin, dOMEGA, deltam=0, montee=Fals
     OME2, AMPL2 = nk.compute_response_curve(T, Vdc, Vac, omega0, M, C, K, OMEGA_fin, OMEGA_debut, -dOMEGA, nb_pts_per, nb_per)
 
     OMEGA_MAX = OME2[np.argmax(AMPL2)]
-    # Chargement des données de la courbe de réponse
-    OMEGA_data, AMPL_data = 0, 0
-    if data:
-        data = np.loadtxt('courbe_reponse_modified.txt', delimiter=',')
-        OMEGA_data, AMPL_data = data[:, 0], data[:, 1]
-    return OMEGA_MAX, OME, AMPL, OME2, AMPL2, OMEGA_data, AMPL_data
+    return OMEGA_MAX, OME, AMPL, OME2, AMPL2
