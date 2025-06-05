df2c7c5655640515a3b39fbf74e9b28c0dd78975
tristanhp <tristan.hausermann-pernet@insa-lyon.fr>
2025-06-05 13:19:35 +0200

    storytelling haha

--- saut_final.py
+++ saut_final.py
@@ -48,32 +48,32 @@ def data_for_saut(delta_m):
     OME4, AMPL4 = nk.recuperer_courbe_data(f'Courbes de réponse/courbe_reponse_masse_{delta_m}_down.txt')
     return OME, AMPL, OME2, AMPL2, OME_data, AMPL_data, OME3, AMPL3, OME4, AMPL4
 
-T, Vdc, Vac, omega0, M, C, K, OMEGA_bal, d = init_params()
+# T, Vdc, Vac, omega0, M, C, K, OMEGA_bal, d = init_params()
 
 
-OMEGA_min = 0.99
-OMEGA_max = 0.992877
-OMEGA_c = (OMEGA_max+OMEGA_min)/2
+# OMEGA_min = 0.99
+# OMEGA_max = 0.992877
+# OMEGA_c = (OMEGA_max+OMEGA_min)/2
 
-periode = 2*np.pi / OMEGA_c  # Period of excitation and response
-nb_pts_per = 50     # Number of points per period for time integration
-dt = periode / nb_pts_per   # Time step size
-nb_per = 500    # Number of periods for time integration
-t_tot = nb_per * periode    # Final time
-t_init = np.array([0.0])               # Initial time
-NT = nb_per * nb_pts_per
-nb_newmark = 750
-deltam = 1e-17
+# periode = 2*np.pi / OMEGA_c  # Period of excitation and response
+# nb_pts_per = 50     # Number of points per period for time integration
+# dt = periode / nb_pts_per   # Time step size
+# nb_per = 500    # Number of periods for time integration
+# t_tot = nb_per * periode    # Final time
+# t_init = np.array([0.0])               # Initial time
+# NT = nb_per * nb_pts_per
+# nb_newmark = 750
+# deltam = 1e-17
 
-liste_dtheta, liste_Yt, liste_dtheta_new, liste_Yt_new = saut_ampl(T, Vdc, Vac, omega0, M, C, K, OMEGA_bal, OMEGA_min, OMEGA_max, nb_pts_per, dt, nb_per, t_init, NT, nb_newmark,deltam)
-OME, AMPL, OME2, AMPL2, OME_data, AMPL_data, OME3, AMPL3, OME4, AMPL4 = data_for_saut(deltam)
+# liste_dtheta, liste_Yt, liste_dtheta_new, liste_Yt_new = saut_ampl(T, Vdc, Vac, omega0, M, C, K, OMEGA_bal, OMEGA_min, OMEGA_max, nb_pts_per, dt, nb_per, t_init, NT, nb_newmark,deltam)
+# OME, AMPL, OME2, AMPL2, OME_data, AMPL_data, OME3, AMPL3, OME4, AMPL4 = data_for_saut(deltam)
 
-fig, ax = plt.subplots(figsize=(10,6))
-ax.plot(liste_dtheta_new, np.abs(liste_Yt_new), label='Déplacement après ajout de masse (Yt_new)')
-ax.plot(liste_dtheta, np.abs(liste_Yt), label='Déplacement sans ajout de masse (Yt)')
-nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OME_data, AMPL_data, ax, 0, True)
-plot_response_curve(OME3, AMPL3, OME4, AMPL4, 0, 0, ax,deltam)
-ax.set_xlabel('OMEGA adimensionné')
-ax.set_ylabel('AMPLITUDE adimensionnée')
-ax.grid()
-plt.show()
+# fig, ax = plt.subplots(figsize=(10,6))
+# ax.plot(liste_dtheta_new, np.abs(liste_Yt_new), label='Déplacement après ajout de masse (Yt_new)')
+# ax.plot(liste_dtheta, np.abs(liste_Yt), label='Déplacement sans ajout de masse (Yt)')
+# nk.plot_response_curve(OME, AMPL, OME2, AMPL2, OME_data, AMPL_data, ax, 0, True)
+# plot_response_curve(OME3, AMPL3, OME4, AMPL4, 0, 0, ax,deltam)
+# ax.set_xlabel('OMEGA adimensionné')
+# ax.set_ylabel('AMPLITUDE adimensionnée')
+# ax.grid()
+# plt.show()
